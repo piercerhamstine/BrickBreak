154a89142bac0c400f51a93aa490b98cbdf3f55b
Pierce <piercerhamstine@gmail.com>
2022-07-11 22:56:04 -0500

    sprite.h, texture.h

--- src/include/texture.h
+++ src/include/texture.h
@@ -1,6 +1,16 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
+#include "stb/stb_image.h"
+#include "glad/gl.h"
+#include <GLFW/glfw3.h>
 
+
+struct Texture
+{
+    GLuint id;
+} typedef Texture;
+
+Texture createTexture();
 
 #endif
