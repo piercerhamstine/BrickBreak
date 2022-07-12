ffaf3da0ad8834610b6a81668db070ec96389a6c
Pierce <piercerhamstine@gmail.com>
2022-07-12 18:45:06 -0500

    texture.c - bindTexture

---
+++ src/texture.c
@@ -0,0 +1,14 @@
+#include "include/texture.h"
+
+Texture createTexture()
+{
+    Texture newTexture;
+
+    return newTexture;
+};
+
+// Bind the provided Texture, all OpenGL Texture operations will apply to the currently bound texture.
+void bindTexture(Texture target)
+{
+    glBindTexture(GL_TEXTURE_2D, target.id);
+};
